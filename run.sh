1fd147a3e3fd754406a82e9abe109d1f40520951
Jonas Lerebours <jonas.lerebours@softbankrobotics.com>
2018-08-14 16:26:39 +0200

    move config file for tests

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ sed 's/#.*//' apt-get.txt | xargs sudo apt-get -y install
 
 pip install -r requirements.txt
 
-python RaspberryTVShowroom/run.py exampleConfig.txt
+python RaspberryTVShowroom/run.py ~/Desktop/RaspberryPlayer/exampleConfig.txt
